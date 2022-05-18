3454a9e01fd93128f425b31a178ca8acb187962b
ATRAPA\dswarandanu <dswarandanu@deloitte.com>
2022-05-18 14:27:22 +0700

    opt to enable redis through config

--- src/main/java/com/deloitte/baseapp/configs/cache/services/RedisService.java
+++ src/main/java/com/deloitte/baseapp/configs/cache/services/RedisService.java
@@ -3,6 +3,7 @@ package com.deloitte.baseapp.configs.cache.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ import java.time.Duration;
 @Slf4j
 @Service
 public class RedisService {
+
+    @Value("${spring.redis.enabled}")
+    private Boolean enabled;
 
     @Qualifier("redisTemplate")
     @Autowired
@@ -23,7 +27,8 @@ public class RedisService {
      * @param value
      */
     public void setValue(final String key, final String value) {
-        redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(1));
+        if (enabled)
+            redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(1));
     }
 
     /**
@@ -32,6 +37,8 @@ public class RedisService {
      * @param key
      */
     public String getValue(final String key) {
+        if (!enabled) return null;
+
         return redisTemplate.opsForValue().get(key);
     }
 
