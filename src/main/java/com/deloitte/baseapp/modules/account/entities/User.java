5c61ff3c818c32d85d22302d136ec7e877ea593b
haziq <mmohdzamani@deloitte.com>
2022-05-19 17:01:19 +0800

    edit user entity to extends genericEntity(might have conflict) add backend to list out all user

--- src/main/java/com/deloitte/baseapp/modules/account/entities/User.java
+++ src/main/java/com/deloitte/baseapp/modules/account/entities/User.java
@@ -1,5 +1,8 @@
 package com.deloitte.baseapp.modules.account.entities;
 
+import com.deloitte.baseapp.commons.GenericEntity;
+import lombok.Data;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,7 +17,8 @@ import javax.validation.constraints.Size;
       @UniqueConstraint(columnNames = "username"),
       @UniqueConstraint(columnNames = "email") 
     })
-public class User {
+@Data
+public class User  implements GenericEntity<User> {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -47,43 +51,17 @@ public class User {
     this.password = password;
   }
 
-  public Long getId() {
-    return id;
+  @Override
+  public void update(User source) {
+    this.username = source.getUsername();
+    this.email = source.getEmail();
+    this.password = source.getPassword();
   }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
+  @Override
+  public User createNewInstance() {
+    User newInstance = new User();
+    newInstance.update(this);
+    return newInstance;
   }
 }
