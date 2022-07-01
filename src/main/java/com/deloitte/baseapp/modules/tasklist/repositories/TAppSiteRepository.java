5658fa693efba937765d570e6eea7a4127b6d1cc
dhamirah <dsamsulkamal@deloitte.com>
2022-07-01 18:06:10 +0800

    Added query parameter for paging and searching

--- src/main/java/com/deloitte/baseapp/modules/tasklist/repositories/TAppSiteRepository.java
+++ src/main/java/com/deloitte/baseapp/modules/tasklist/repositories/TAppSiteRepository.java
@@ -1,11 +1,15 @@
 package com.deloitte.baseapp.modules.tasklist.repositories;
 
-
 import com.deloitte.baseapp.commons.GenericRepository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import com.deloitte.baseapp.modules.tasklist.entities.TAppSite;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface TAppSiteRepository extends GenericRepository<TAppSite> {
+    @Query("from TAppSite s where lower(s.cd) LIKE concat('%', lower(:siteId), '%')")
+    Page<TAppSite> findAll(Pageable pageable, @RequestParam String siteId);
 }
